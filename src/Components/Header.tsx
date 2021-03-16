a7f3ad11256f877462f63441101ffcd3b365c06b
g3vxy <g3vxyv@gmail.com>
2021-03-16 18:21:15 +0300

    Hamburger menu in progress

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,11 +4,16 @@ import {
 } from "react-router-dom"
 
 const Header = () => {
+
     return <div className="header">
-                <div className="blur"></div>
                 <h2 className="logo">
                     <Link to="/">GameLog</Link>
                 </h2>
+                <button className="hamburger" onClick={() => {
+                    document.getElementsByClassName("navigation")[0].classList.toggle("show")
+                }}>
+                    &#9776;
+                </button>
                 <div className="navigation">
                     <Link to="/pricing">Pricing</Link>
                     <Link to="/features">Features</Link>
